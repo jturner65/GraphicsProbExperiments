e7f9700de3b0ec708a06653f5c0cafff5ab54e55
John Turner <7strbass@gmail.com>
2022-11-16 20:56:01 -0500

    --move KDTree to Utils library

--- src/base_RayTracer/scene/photonMapping/myPhoton.java
+++ src/base_RayTracer/scene/photonMapping/myPhoton.java
@@ -1,5 +1,8 @@
 package base_RayTracer.scene.photonMapping;
 
+import base_Utils_Objects.kdTree.base.Base_KDObject;
+import base_Utils_Objects.kdTree.base.Base_KDTree;
+
 /**
  * Photon class, holds photon power.  Placed in KDNode upon construction of KDTree
  * @author 7strb
@@ -9,7 +12,7 @@ public class myPhoton extends Base_KDObject<myPhoton>{
 //	public myVector dir;				//direction of photon on surface TODO
 	public double[] pwr;				//power of light of this photon in r,g,b	
 	
-	public myPhoton (KDTree<myPhoton> _tree, double[] _pwr, double x, double y, double z) {
+	public myPhoton (Base_KDTree<myPhoton> _tree, double[] _pwr, double x, double y, double z) {
 		super(_tree, new double[]{x,y,z});
 		pwr = _pwr; 
 	}
