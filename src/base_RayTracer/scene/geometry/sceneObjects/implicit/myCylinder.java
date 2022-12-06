4850125cd04510c04b33cff3453b1add6caf3f77
John Turner <7strbass@gmail.com>
2022-12-06 11:02:09 -0500

    -- make Abstract base class for cylinders to hold common functionality

--- src/base_RayTracer/scene/geometry/sceneObjects/implicit/myCylinder.java
+++ src/base_RayTracer/scene/geometry/sceneObjects/implicit/myCylinder.java
@@ -3,18 +3,22 @@ package base_RayTracer.scene.geometry.sceneObjects.implicit;
 import base_RayTracer.ray.rayCast;
 import base_RayTracer.ray.rayHit;
 import base_RayTracer.scene.base.Base_Scene;
-import base_RayTracer.scene.geometry.base.Geom_ObjType;
+import base_RayTracer.scene.geometry.base.GeomObjType;
+import base_RayTracer.scene.geometry.sceneObjects.implicit.base.Base_Cylinder;
 import base_Math_Objects.matrixObjs.doubles.myMatrix;
 import base_Math_Objects.vectorObjs.doubles.myPoint;
 import base_Math_Objects.vectorObjs.doubles.myVector;
 
-public class myCylinder extends myHollow_Cylinder{
+/**
+ * Class defining a capped cylinder via implicit equation
+ * @author 7strb
+ *
+ */
+public class myCylinder extends Base_Cylinder{
 	private double[][] capEqs;
   
 	public myCylinder(Base_Scene _p, double _myRadius, double _myHeight, double x, double y, double z, double xO,double yO, double zO){
-		super(_p, _myRadius, _myHeight, x, y, z, xO, yO, zO);
-		type = Geom_ObjType.Cylinder;
-		
+		super(_p, _myRadius, _myHeight, x, y, z, xO, yO, zO, GeomObjType.Cylinder);		
 		//cap eqs are planar equations for endcaps of cylinder - treat xO,yO,zO (orientation vector) as direction of top cap 
 		double [] topCapEq = new double[4], btmCapEq = new double[4];		//ax + by + cz + d = 0 : a,b,c are normal, d is -distance from origin
 		topCapEq[0] = xO;    topCapEq[1] = yO; 	topCapEq[2] = zO;
@@ -27,49 +31,40 @@ public class myCylinder extends myHollow_Cylinder{
 		postProcBBox();				//cnstrct and define bbox
 	}
  
-	//check if passed ray intersects with this cylinder - first using x/z for circular intersection, then planar intersection with end caps, then check which is closest and positive  
-	public rayHit intersectCheck(rayCast _ray, rayCast transRay, myMatrix[] _ctAra){
-		myPoint pC = originRadCalc(transRay);
-		double a = getAVal(transRay),b = getBVal(transRay, pC), c = getCVal(transRay, pC);
-		double discr = ((b*b) - (4*a*c));
-		//quadratic - check first if imaginary - if so then no intersection
-		if (!(discr < 0)){//real roots exist - means ray hits x-z walls somewhere
-			//find values of t - want those that give largest value of z, to indicate the closest intersection to the eye
-			double discr1 = Math.pow(discr,.5),t1 = (-b + discr1)/(2*a),t2 = (-b - discr1)/(2*a);
-			//set the t value of the intersection to be the minimum of these two values (which would be the edge closest to the eye/origin of the ray)
-			// (the other value, if one exists, is on the other side of the sphere)
-			double cyltVal = Math.min(t1,t2), cyltOtr = Math.max(t1, t2);
-			if (cyltVal < epsVal){//if min less than 0 then that means it intersects behind the viewer.  pick other t
-				cyltOtr = cyltVal;
-				cyltVal = Math.max(t1, t2);				
-				if (cyltVal < epsVal){//if still less than 0 then that means both intersections behind ray origin - this isn't an intersection, dont draw
-					return new rayHit(false);	
-				}//if both t's are less than 0 then don't paint anything
-			}//if the min t val is less than 0
-			
-			//check if caps instead of walls - intersect t for plane should be between tVal and tOtr	
-			boolean planeRes = true;
-			double[] num = new double[]{0,0}, denom = new double[]{1,1}, pl_tVal = new double[]{0,0};
-			for(int i = 0; i< capEqs.length;++i){
-				denom[i]=capEqs[i][0]*transRay.direction.x + capEqs[i][1]*transRay.direction.y + capEqs[i][2]*transRay.direction.z;
-				if(Math.abs(denom[i]) > epsVal) {
-					num[i]=capEqs[i][0]*transRay.origin.x + capEqs[i][1]*transRay.origin.y + capEqs[i][2]*transRay.origin.z + capEqs[i][3];
-					pl_tVal[i] = -num[i]/denom[i];
-				} else {					pl_tVal[i] = 10000;}
-			}
-			double pltVal = Math.min(pl_tVal[0],pl_tVal[1]);
-			int idxVis = (pltVal == pl_tVal[0] ? 0 : 1);
-			if (pltVal < 0){//if min less than 0 then that means it intersects behind the viewer.  pick other t
-				pltVal = pl_tVal[idxVis];
-				if (pltVal < epsVal){		planeRes = false;	}//if both t's are less than 0 then didn't hit caps within circular bounds
-			}//if the min t val is less than 0		
-			double tVal = 0, maxCylT = Math.max(cyltVal, cyltOtr), minCylT = Math.min(cyltVal, cyltOtr);
-			if(planeRes && (((minCylT <= 0)					//inside cylinder, 
-				&& (pltVal >= -epsVal) && (pltVal <= maxCylT)) || ((pltVal > minCylT) && (pltVal <= maxCylT)))) {		tVal = pltVal;} 
-			else {					tVal = cyltVal;					idxVis = 2;	}
-			double yInt1 = transRay.origin.y + (tVal * transRay.direction.y);			
-			if((yInt1+epsVal >= yBottom ) && (yInt1-epsVal <= yTop)){return transRay.objHit(this, _ray.direction, _ctAra, transRay.pointOnRay(tVal),new int[]{idxVis},tVal);} 
-		}	
+	/**
+	 * Check specific cylinder instance for ray hit.
+	 * @param _ray ray cast
+	 * @param _transRay transformed ray cast
+	 * @param _ctAra transformation matrix for the ray
+	 * @param cyltVal closest t value to origin of ray
+	 * @param cyltOtr furthest t value from origin of ray
+	 * @return
+	 */
+	@Override
+	protected final rayHit intersectCheck_Indiv(rayCast _ray, rayCast transRay, myMatrix[] _ctAra, double cyltVal, double cyltOtr){	
+		//check if caps instead of walls - intersect t for plane should be between tVal and tOtr	
+		boolean planeRes = true;
+		double[] num = new double[]{0,0}, denom = new double[]{1,1}, pl_tVal = new double[]{0,0};
+		for(int i = 0; i< capEqs.length;++i){
+			denom[i]=capEqs[i][0]*transRay.direction.x + capEqs[i][1]*transRay.direction.y + capEqs[i][2]*transRay.direction.z;
+			if(Math.abs(denom[i]) > epsVal) {
+				num[i]=capEqs[i][0]*transRay.origin.x + capEqs[i][1]*transRay.origin.y + capEqs[i][2]*transRay.origin.z + capEqs[i][3];
+				pl_tVal[i] = -num[i]/denom[i];
+			} else {					pl_tVal[i] = 10000;}
+		}
+		double pltVal = Math.min(pl_tVal[0],pl_tVal[1]);
+		int idxVis = (pltVal == pl_tVal[0] ? 0 : 1);
+		if (pltVal < 0){//if min less than 0 then that means it intersects behind the viewer.  pick other t
+			pltVal = pl_tVal[idxVis];
+			if (pltVal < epsVal){		planeRes = false;	}//if both t's are less than 0 then didn't hit caps within circular bounds
+		}//if the min t val is less than 0		
+		double tVal = 0, maxCylT = Math.max(cyltVal, cyltOtr), minCylT = Math.min(cyltVal, cyltOtr);
+		if(planeRes && (((minCylT <= 0)					//inside cylinder, 
+			&& (pltVal >= -epsVal) && (pltVal <= maxCylT)) || ((pltVal > minCylT) && (pltVal <= maxCylT)))) {		tVal = pltVal;} 
+		else {					tVal = cyltVal;					idxVis = 2;	}
+		double yInt1 = transRay.origin.y + (tVal * transRay.direction.y);			
+		if((yInt1+epsVal >= yBottom ) && (yInt1-epsVal <= yTop)){return transRay.objHit(this, _ray.direction, _ctAra, transRay.pointOnRay(tVal),new int[]{idxVis},tVal);} 
+		
 		return new rayHit(false);	 
 	}//intersectCheck method
   	//returns surface normal of cylinder at given point on cylinder
@@ -94,6 +89,7 @@ public class myCylinder extends myHollow_Cylinder{
   				res+="" + capEqs[i][j]+ eqvals[j];
   			}
   		}
+  		res+="\n";
   		return res;
   	}
 }//class myCylinder
--- src/base_RayTracer/scene/geometry/sceneObjects/implicit/myHollow_Cylinder.java
+++ src/base_RayTracer/scene/geometry/sceneObjects/implicit/myHollow_Cylinder.java
@@ -3,52 +3,48 @@ package base_RayTracer.scene.geometry.sceneObjects.implicit;
 import base_RayTracer.ray.rayCast;
 import base_RayTracer.ray.rayHit;
 import base_RayTracer.scene.base.Base_Scene;
-import base_RayTracer.scene.geometry.base.Geom_ObjType;
-import base_RayTracer.scene.geometry.sceneObjects.implicit.base.Base_ImplicitObject;
+import base_RayTracer.scene.geometry.base.GeomObjType;
+import base_RayTracer.scene.geometry.sceneObjects.implicit.base.Base_Cylinder;
 import base_Math_Objects.matrixObjs.doubles.myMatrix;
 import base_Math_Objects.vectorObjs.doubles.myPoint;
 import base_Math_Objects.vectorObjs.doubles.myVector;
-import processing.core.PImage;
 
-public class myHollow_Cylinder extends Base_ImplicitObject{
-	protected double myHeight, yTop, yBottom;
+/**
+ * Class defining a hollow tube via implicit equation
+ * @author 7strb
+ *
+ */
+public class myHollow_Cylinder extends Base_Cylinder{
 
 	public myHollow_Cylinder(Base_Scene _p, double _myRadius, double _myHeight, double x, double y, double z, double xO,double yO, double zO) {
-		super(_p, x,y,z);
-		type = Geom_ObjType.Hollow_Cylinder;
-		radX= _myRadius;
-		radZ = _myRadius;
-		myHeight = _myHeight;
-		yTop = origin.y + myHeight;//top of can
-		yBottom = origin.y; //bottom of can
-	    minVals = getMinVec();
-	    maxVals = getMaxVec();	    
+		super(_p, _myRadius, _myHeight, x, y, z, xO, yO, zO, GeomObjType.Hollow_Cylinder);
 		postProcBBox();				//cnstrct and define bbox
 	}
 	 
-	//check if passed ray intersects with this cylinder - first using x/z for circular intersection, then planar intersection with end caps, then check which is closest and positive  
-	public rayHit intersectCheck(rayCast _ray, rayCast transRay, myMatrix[] _ctAra){	
-		myPoint pC = originRadCalc(transRay);
-		double a = getAVal(transRay), b = getBVal(transRay, pC), c = getCVal(transRay, pC), discr = ((b*b) - (4*a*c));
-		//quadratic - check first if imaginary - if so then no intersection
-		if (!(discr < 0)){//real roots exist - means ray hits x-z walls somewhere
-			double discr1 = Math.pow(discr,.5),t1 = (-b + discr1)/(2*a),t2 = (-b - discr1)/(2*a);
-			double cyltVal = Math.min(t1,t2), cyltOtr = Math.max(t1, t2);
-			if (cyltVal < -epsVal){//if min less than 0 then that means it intersects behind the viewer.  pick other t
-				double tmp = cyltOtr; 	cyltOtr = cyltVal;	cyltVal = tmp;				
-				if (cyltVal < -epsVal){		return new rayHit(false);		}//if both t's are less than 0 then don't paint anything
-			}//if the min t val is less than 0
-			//light very near to bounds of cylinder, need check to avoid shadow shennanigans
-			double yInt1 = transRay.origin.y + (cyltVal * transRay.direction.y);
-			//in args ara 0 means hitting outside of cylinder, 1 means hitting inside
-			if((cyltVal > epsVal) && (yInt1 > yBottom ) && (yInt1 < yTop)){return transRay.objHit(this, _ray.direction, _ctAra, transRay.pointOnRay(cyltVal),new int[]{0},cyltVal);}
-			double yInt2 = transRay.origin.y + (cyltOtr * transRay.direction.y);
-			if((cyltOtr > epsVal) && (yInt2 > yBottom ) && (yInt2 < yTop)){return transRay.objHit(this, _ray.direction, _ctAra, transRay.pointOnRay(cyltOtr),new int[]{1},cyltOtr);}
-		} 
+	/**
+	 * Check specific cylinder instance for ray hit.
+	 * @param _ray ray cast
+	 * @param _transRay transformed ray cast
+	 * @param _ctAra transformation matrix for the ray
+	 * @param cyltVal closest t value to origin of ray
+	 * @param cyltOtr furthest t value from origin of ray
+	 * @return
+	 */
+	@Override
+	protected final rayHit intersectCheck_Indiv(rayCast _ray, rayCast transRay, myMatrix[] _ctAra, double cyltVal, double cyltOtr){	
+		//light very near to bounds of cylinder, need check to avoid shadow shennanigans
+		double yInt1 = transRay.origin.y + (cyltVal * transRay.direction.y);
+		//in args ara 0 means hitting outside of cylinder, 1 means hitting inside
+		if((cyltVal > epsVal) && (yInt1 > yBottom ) && (yInt1 < yTop)){return transRay.objHit(this, _ray.direction, _ctAra, transRay.pointOnRay(cyltVal),new int[]{0},cyltVal);}
+		double yInt2 = transRay.origin.y + (cyltOtr * transRay.direction.y);
+		if((cyltOtr > epsVal) && (yInt2 > yBottom ) && (yInt2 < yTop)){return transRay.objHit(this, _ray.direction, _ctAra, transRay.pointOnRay(cyltOtr),new int[]{1},cyltOtr);}
+
 		return new rayHit(false);	    
 	}//intersectCheck method
 
-  	//returns surface normal of cylinder at given point on cylinder	
+  	/**
+  	 * returns surface normal of cylinder at given point on cylinder	
+  	 */
   	@Override
   	public myVector getNormalAtPoint(myPoint pt,  int[] args){//in args ara 0 means hitting outside of cylinder, 1 means hitting inside	
   		myVector result= (args[0] == 1) ? new myVector((origin.x - pt.x), 0, (origin.z - pt.z)) : new myVector((pt.x - origin.x), 0, (pt.z - origin.z));
@@ -58,36 +54,6 @@ public class myHollow_Cylinder extends Base_ImplicitObject{
   		return result;
   	}//method getNormalAtPoint  		
 
-	//find the u (x) value in a texture to plot to a specific point on the object	TODO
-	@Override
-	protected double findTextureU(myPoint isctPt, double v, PImage myTexture, double time){double u = 0.0;return u;}     	
-  	//find the v (y) value in a texture to plot to a specific point on the object  	TODO
-	@Override
-	protected double findTextureV(myPoint isctPt, PImage myTexture, double time){		double v = 0.0; 		return v; 	}   
-	
-  	public double getMyHeight(){  return myHeight;}  
-  	
-  	// calculates the "A" value for the quadratic equation determining the potential intersection of the passed ray with a cylinder of given radius and center
-  	public double getAVal(rayCast _ray){return ((_ray.direction.x/radX) * (_ray.direction.x/radX)) + ((_ray.direction.z/radZ) * (_ray.direction.z/radZ));  	}  
-  	//calculates the "B" value for the quadratic equation determining the potential intersection of the passed ray with a cylinder of given radius and center
-  	public double getBVal(rayCast _ray, myPoint pC){return 2*(((_ray.direction.x/radX) * pC.x) + ((_ray.direction.z/radZ) * pC.z)); }  
-  	//calculates the "C" value for the quadratic equation determining the potential intersection of the passed ray with a cylinder of given radius and center
-  	public double getCVal(rayCast _ray, myPoint pC){return (pC.x * pC.x) + (pC.z * pC.z) - 1; }  
-
-	@Override
-	public myVector getMaxVec(){
-		myVector res = new myVector(origin);
-		double tmpVal = radX+radZ;
-		res._add(tmpVal,myHeight,tmpVal);
-		return res;
-	}	
-	@Override
-	public myVector getMinVec(){
-		myVector res = new myVector(origin);
-		double tmpVal = radX+radZ;
-		res._add(-tmpVal,0,-tmpVal);
-		return res;
-	}	
-  	public String toString(){  return super.toString() + "\n"+ type +"-Specific : Height : " + myHeight + " radius : [" + radX+ "|" +radZ+"] y End : " + yBottom; 	}
+  	public String toString(){  return super.toString() + "\n"+ type +"-Specific : Height : " + myHeight + " radius : [" + radX+ "|" +radZ+"] y End : " + yBottom +"\n"; 	}
 
 }//myHollow_Cylinder
